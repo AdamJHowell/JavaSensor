1d95086e8f8f9387073a45737a92c0d3ad0dd48b
Adam Howell <bigguywhokills@yahoo.com>
2014-07-08 16:46:44 -0600

    Initial commit

---
+++ accelerometerLayout.java
@@ -0,0 +1,17 @@
+/*We will build an application which will change its background color if it is shuffled. Create a new Android project called de.vogella.android.sensor with an activity called SensorTestActivity.
+
+Change your layout file to the following code.*/
+
+<?xml version="1.0" encoding="utf-8"?>
+<LinearLayout xmlns:android="http://schemas.android.com/apk/res/android"
+    android:layout_width="match_parent"
+    android:layout_height="match_parent"
+    android:orientation="vertical" >
+
+    <TextView
+        android:id="@+id/textView"
+        android:layout_width="match_parent"
+        android:layout_height="match_parent"
+        android:text="Shake to get a toast and to switch color" />
+
+</LinearLayout>
